89976662ef973e23b1150a780483e507afe4a31b
jassusahu <128967308+jassusahu@users.noreply.github.com>
2023-03-26 15:51:07 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+
+$con=new mysqli('localhost','root','za81as@#1212','Users_Database' );
+if(!$con){
+    die(mysqli_error($con));
+}
+
+?>
---
+++ update.php
@@ -0,0 +1,72 @@
+<?php
+include 'connect.php';
+$id=$_GET['updateid'];
+$sql="select * from `Users` where id=$id";
+$result=mysqli_query($con,$sql);
+$row=mysqli_fetch_assoc($result);
+$name=$row['name'];
+$email=$row['email'];
+$mobile=$row['mobile'];
+$password=$row['password'];
+
+if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $mobile=$_POST['mobile'];
+    $password=$_POST['password'];
+
+    $sql="update `Users` set id=$id,name='$name',email='$email',mobile='$mobile',password='$password' where id=$id";
+    $result=mysqli_query($con,$sql);
+    if($result){
+        echo "updated successfully";
+       // header('location:display.php');
+
+    }
+else{
+    die(mysqli_error($con));
+}
+
+
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+    <title>usermanament system</title>
+  </head>
+  <body>
+    <div class="container my-5">
+    <form method="post">
+  <div class="form-group">
+    <label>Name</label>
+    <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="off">
+    </div>
+    <form method="post">
+  <div class="form-group">
+    <label>Email</label>
+    <input type="email" class="form-control" placeholder="Enter your email" name="email">
+    </div>
+    <form method="post">
+  <div class="form-group">
+    <label>Mobile</label>
+    <input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile">
+    </div>
+    <form method="post">
+  <div class="form-group">
+    <label>Password</label>
+    <input type="text" class="form-control" placeholder="Enter your Password" name="password">
+    </div>
+  
+  <button type="submit" class="btn btn-primary" name="submit">update</button>
+</form>
+   
+  </body>
+</html>
